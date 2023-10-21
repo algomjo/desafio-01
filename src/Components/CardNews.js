faf9d80f98cc60b3a2b564295fdb681788ea8b19
Alexandre Gomes <algomjo@live.com>
2023-10-21 11:42:28 -0300

    21/10/2023

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -15,10 +15,14 @@ class CardNews extends HTMLElement {
         cardLeft.setAttribute("class", "card__left");
 
         const autor = document.createElement("span");
-        autor.textContent = "By " + this.getAttribute("autor");
+        autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
 
         const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url");
+        
         const newsContent = document.createElement("p");
+        newsContent.textContent = this.getAttribute("content");
 
         cardLeft.appendChild(autor);
         cardLeft.appendChild(linkTitle);
@@ -26,7 +30,11 @@ class CardNews extends HTMLElement {
 
         const cardRight = document.createElement("div");
         cardRight.setAttribute("class", "card__right");
+
         const newsImage = document.createElement("img");
+        newsImage.src = this.getAttribute("photo") || "assets/photo.default.jpg";
+        newsImage.alt = "Vader"
+
         cardRight.appendChild(newsImage);
 
         componentRoot.appendChild(cardLeft);
@@ -36,7 +44,43 @@ class CardNews extends HTMLElement {
     }
 
     styles(){
+        const style = document.createElement("style");
+        style.textContent = `
+        .card{
+            width: 80%;
+            box-shadow: 10px 10px 18px -6px rgba(0,0,0,0.75);
+            -webkit-box-shadow: 10px 10px 18px -6px rgba(0,0,0,0.75);
+            -moz-box-shadow: 10px 10px 18px -6px rgba(0,0,0,0.75);
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+        }
+        
+        .card__left{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            padding-left: 10px;
+        }
+        
+        .card__left > span {
+            font-weight: 400;
+        }
+        
+        .card__left > a {
+            margin-top: 15px;
+            font-size: 25px;
+            color: black;
+            text-decoration: none;
+            font-weight: bold;
+        }
+        
+        .card__left > p{
+            color: rgb(70, 70, 70);
+        }
+        `
 
+        return style;
     }
 }
 
